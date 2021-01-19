7aff708a3cadadd5df14e4ff5403279e1ed5ab74
Germain kataku <katakugermain@gmail.com>
2021-01-19 19:37:43 +0100

    edit partenaire

--- administration/views.py
+++ administration/views.py
@@ -1,3 +1,62 @@
 from django.shortcuts import render
 
 # Create your views here.
+def funcname(request):
+    """
+    docstring
+    """
+    pass
+
+def funcname(request):
+    """
+    docstring
+    """
+    pass
+
+def funcname(request):
+    """
+    docstring
+    """
+    pass
+
+def funcname(request):
+    """
+    docstring
+    """
+    pass
+
+def funcname(request):
+    """
+    docstring
+    """
+    pass
+
+def funcname(request):
+    """
+    docstring
+    """
+    pass
+
+def funcname(request):
+    """
+    docstring
+    """
+    pass
+
+def funcname(request):
+    """
+    docstring
+    """
+    pass
+
+def funcname(request):
+    """
+    docstring
+    """
+    pass
+
+def funcname(request):
+    """
+    docstring
+    """
+    pass
